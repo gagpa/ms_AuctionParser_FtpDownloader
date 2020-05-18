fc8855c2882738f19f6513c60f2fac4f9faed7a0
gagpa <speitshot@gmail.com>
2020-05-18 15:51:51 +0300

    Дополена инфомарция о МикроСервисе

--- ms_app/configs/package_configs/ftp44_config.py
+++ ms_app/configs/package_configs/ftp44_config.py
@@ -4,5 +4,5 @@ from os import environ
 class ConfigFtp44:
     host = environ.get('FTP_HOST')
     login = environ.get('FTP_LOGIN')
-    password = environ.get('FTP_PASS')
+    password = environ.get('FTP_PASSWORD')
     zips = environ.get('FTP_ZIPS')
